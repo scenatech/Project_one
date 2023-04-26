ddcea230de03f28c9958ae7414a32e728518ddd9
senahtech <senagamos@gmail.com>
2023-04-26 13:01:19 -0700

    My first commit

---
+++ my_to_do.md
@@ -0,0 +1,31 @@
+## Learning Git, Github and its Commands
+
+#--------console commands---------#
+1. cd = change directory
+2. ls = list
+3. clear = clear terminal or console
+4. pwd = print working directory
+5. mkdir = make new directory
+6. touch = use to create new file
+7. cd .. = navigating back
+8. cd ~  = navigating back to home page
+9. mv =use to rename and move file
+10. rm  (-R) = use to remove file or directory
+
+
+#------------Git commands------------#
+1. git clone = Is command that creates a copy of a remote Git repository on your local machine. 
+2. cd  = change directory
+3. ls = list
+
+4. git status = Is command that shows the current state of your working directory and the staging area. It shows which files have been modified, added, or deleted since the last commit, and which files are currently staged for the next commit.
+
+5. git add . =  Git command that adds changes to the staging area. The staging area is a temporary storage area where changes are stored before they are committed to the repository.
+
+6. git commit -m " " =You can also provide a commit message that describes the changes you have made.
+
+7. git push =Git command that sends the committed changes in your local repository to a remote repository.
+
+
+
+
---
+++ start.py
@@ -0,0 +1,149 @@
+#Example 1
+
+#Printing anything general using print function
+print("Hello world")
+print('The headmaster was not in class last time')
+# --------------------------------------------------------------
+
+# Example 2
+#Using VARIABLE
+students2 ='benjamin,mavis'
+students3 ="kwame,godwin"
+# --------------------------------------------------------------
+
+
+# Example 3
+#using a DESCRIPTIVE name
+grocery_store = 'fruit juice,mango'
+grocery_store2 = ("banana, orange")
+# --------------------------------------------------------------
+
+# Example 4
+AGE = 24
+# --------------------------------------------------------------
+
+
+#Example 5
+kofi_store = ('banana','mango','apple','watermelon')
+ama_store =('pineapple','guava','apple','orange')
+# --------------------------------------------------------------
+
+
+#Data types with examples
+age = 22 #int data type
+
+kofi_is_a_male = True #boolean data type
+
+price_of_gari = 22.00 #float data type
+# --------------------------------------------------------------
+
+
+hobby =('sena likes gardening') #string data type
+
+store = {'rice': 22,'oil':1} #dict data type
+
+grocery_store = ['mango','apple','kiwi'] #list data type
+
+students_attributes = ('male',22,25.5,'fisrt_class','lecturers') #tuple data type
+# --------------------------------------------------------------
+
+#This program takes user input and print on console
+name = input('what is your name: ') 
+age = int(input('please enter your age: '))
+# print(name,age)
+# --------------------------------------------------------------
+
+
+#This program concatenates two inputs(hard code)
+name = 'sena'
+age = 18
+
+result = ('my name is '+ name+ ' and i am '+ str(age)+' years old')
+# --------------------------------------------------------------
+
+
+#This program takes user inputs and concatenates it
+name = input('please enter your name: ')
+age = int(input("please enter your age: "))
+
+result = ('Welcome '+ name+ ' and i am '+ str(age)+' years old')
+# --------------------------------------------------------------
+
+
+#This program takes Int and Float value(Hard code)
+first_number = 22
+second_number = 22.3
+
+final_answer =first_number + second_number
+# --------------------------------------------------------------
+
+#This program takes Int and Float value from a user
+first_number = int(input('please enter your first number: '))
+second_number =float(input('please enter your second number: '))
+
+result = ('your final answer is ' + str(first_number + second_number))
+print(result)
+# --------------------------------------------------------------
+
+#How to check for the type of built in data type
+#Example 
+my_list = [2,3,4]
+print(type(my_list))
+# --------------------------------------------------------------
+
+#How to check for the attributes of built in data types
+#Example
+print(dir(my_list))
+# --------------------------------------------------------------
+
+#How to use append data type to add to a list(hard code)
+grocery_store = ['mango', 'juice','pineapple','apple']
+# my_list2 = ('kofi','ama')
+
+
+grocery_store.append('banana')
+print(grocery_store)
+# --------------------------------------------------------------
+
+
+#How to use append data type to add to a list
+grocery_store = ['mango', 'juice','pineapple','apple']
+# my_list2 = ('kofi','ama')
+
+user = input('please enter your choice of fruit: ')
+
+grocery_store.append(user)
+print(grocery_store)
+# --------------------------------------------------------------
+
+
+#How to use .append and input to get user input
+grocery_store = []
+
+
+user2 =int(input('Please enter the number of fruit: '))
+user = input('Please enter your choice of fruit: ')
+
+
+grocery_store.append(user2)
+grocery_store.append(user)
+
+
+print(grocery_store)
+#-----------------------------------------------------------
+
+
+
+
+
+
+
+#using PRINT function to print students
+# print(students2,students3)
+# print(grocery_store)
+# print(kofi_store,ama_store)
+# print(kofi_store[1])
+# print(ama_store[3])
+print(result)
+
+
