47ed7f1056bc72bfcc2d99f7fcf51d3624478bac
senahtech <>
2023-05-08 12:46:36 -0700

    Personal studies

--- practice.py
+++ practice.py
@@ -1,83 +1,33 @@
-# #Printing anything general using print function
-# print('Hello world!')
-# print("Celestine is performing today")
+grocery_store = ["mango","juice","pear","pineapple"]
 
-# #Using VARIABLE
-# students = 'jecoliah,naa,shelter'
-# students2 = "kofi,ama,afua,sena,kwaku"
-# print(students,students2)
+# grocery_store.insert(1, "kiwi")
 
-# #Using a DESCRIPTIVE name
-# ark_medical_centre = 'gynae,OPD,records'
-# danfa_health_clinic = "eye clinic,lab,maternity"
+# user = input('Please enter your choice of fruit: ')
 
-# #Constant
-# AGE = 25
+# grocery_store.insert(1,user )
 
-# #Tuple data type
-# kofi_store = ('mango','apple','orange',)
-# ama_store = ('kiwi','guava')
+# grocery_store.clear()
 
-# age = 21 #int data type
+# print(grocery_store.index('juice'))
 
 
-# price_of_gari = 15.50 #Float data type
 
-# grocery_store = ['star fruit','apple','guava']
-
-# ama_is_a_female = True#Boolean data type
-
-# hobby = ('Sena loves gardening')
+grocery_store.pop(3)
 
 
-#This program  takes a user input and print on console
-# name = input('Please enter your name: ')
-# age = int(input('Please enter your age: '))
-# print(name,age)
 
 
-#This program concatenates two inputs(HC)
-# name = 'worlanyoh'
-# age = 25
-
-# result = ('my name  is '  +  name  +  ' and i  am '  +  str(age)  +  'years old')
-
-# print(result)
-
-# #This program takes Int and Float value from a user
-# first_number = int(input('Please enter your first number: '))
-# second_number =float(input('Please enter your second number: '))
-
-# result = ('your final answer is ' + str(first_number + second_number))
-# print(result)
-
-# my_list =[2,3,4,5]
-# print(type(my_list))
 
 
-# my_list = [2,3,4]
-# print(dir(my_list))
 
-#How to use append data type to add to a list(HC)
-grocery_store = ['mango','apple','kiwi','blueberry']
 
-user = input(('Please enter your choice fruit: '))
 
-grocery_store.append(user)
+
+
 
 print(grocery_store)
 
-#How to use .append and input to get user input
-grocery_store = []
 
-user = int(input('Please enter the number of fruit:'))
-
-user2 = input('Please enter your choice of fruit: ')
-
-grocery_store.append(user)
-grocery_store.append(user2)
-grocery_store.append(user2)
-grocery_store.append(user2)
 
 
 
